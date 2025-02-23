0f15c66b1750152885a8f5fee4e511878618be46
Ruslan Dolibskyi <dolibskiyruslan.gmail.com>
2025-02-23 12:03:12 +0200

    add branch 03-postgresql to git

---
+++ db/Sequelize.js
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize";
+
+const User = sequelize.define(
+    'Contact', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    favorite: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+}
+
+);
